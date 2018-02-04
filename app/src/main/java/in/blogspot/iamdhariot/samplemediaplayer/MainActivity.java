dbcc223c6404644dd8afaa15b1f07d2e7361a9f9
iamdhariot <dharm12997@gmail.com>
2018-02-04 11:25:28 +0530

    This is my first git push.

---
+++ app/src/main/java/in/blogspot/iamdhariot/samplemediaplayer/MainActivity.java
@@ -0,0 +1,40 @@
+package in.blogspot.iamdhariot.samplemediaplayer;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnPlay,btnPause;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this,R.raw.ncs_earth);
+
+        btnPlay = (Button)findViewById(R.id.btnPlay);
+        btnPause = (Button)findViewById(R.id.btnPause);
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                mediaPlayer.start();
+            }
+        });
+
+        btnPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.pause();
+
+            }
+        });
+
+    }
+}
