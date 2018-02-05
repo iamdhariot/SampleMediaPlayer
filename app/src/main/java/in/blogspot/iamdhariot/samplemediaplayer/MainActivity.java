83c85e9de8dc5379a2b52a1adeed15913c1f5ef9
iamdhariot <dharm12997@gmail.com>
2018-02-05 11:41:42 +0530

    This is my first git push.

--- app/src/main/java/in/blogspot/iamdhariot/samplemediaplayer/MainActivity.java
+++ app/src/main/java/in/blogspot/iamdhariot/samplemediaplayer/MainActivity.java
@@ -42,7 +42,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnReset.setOnClickListener(this);
 
     }
-
     /**
      * Btn onClick events
      * */
